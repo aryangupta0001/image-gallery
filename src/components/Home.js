746b79b8c08c1307d628775b91fe7550415594a9
Aryan Gupta <aryanguptabareilly067@gmail.com>
2023-09-16 02:11:10 +0530

    title, search bar, categories added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>
-        <h1>
-            Snapshot
-        </h1>
-    </div>
-  )
+    return (
+        <>
+            <h1 id='title'>
+                Snapshot
+            </h1>
+
+            <div className='d-flex' id='searchBar'>
+                <input type="text" placeholder='Search' id='searchBox' />
+                <i class="fa-solid fa-magnifying-glass" id='searchButton' />
+            </div>
+
+            <div id="quickSearches" className='d-flex'>
+                <div className="searchCategories">Mountain</div>
+                <div className="searchCategories">Beaches</div>
+                <div className="searchCategories">Birds</div>
+                <div className="searchCategories">Food</div>
+            </div>
+        </>
+    )
 }
 
 export default Home
